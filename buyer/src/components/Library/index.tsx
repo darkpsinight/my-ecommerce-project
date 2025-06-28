28103b5e75e675df408527c466b0cd2c8952d8a8
darkpsinight <barhoumi.ragheb@gmail.com>
2025-06-28 20:28:20 +0100

    update icon again

--- buyer/src/components/Library/index.tsx
+++ buyer/src/components/Library/index.tsx
@@ -21,6 +21,7 @@ import LibraryFilters from "./LibraryFilters";
 import LibraryStats from "./LibraryStats";
 import GiftManagement from "./GiftManagement";
 import CodeModal from "./CodeModal";
+import { FaGift } from "react-icons/fa";
 
 const Library = () => {
   const { token } = useSelector((state: any) => state.authReducer);
@@ -227,10 +228,7 @@ const Library = () => {
                   }`}
                 >
                   <div className="flex items-center gap-2">
-                    <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clipRule="evenodd" />
-                      <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z" />
-                    </svg>
+                  <FaGift className="w-5 h-5"/>
                     Gift Management
                     <span className="text-xs bg-yellow-light-2 text-yellow-dark px-2 py-0.5 rounded-full ml-1">
                       Coming Soon
